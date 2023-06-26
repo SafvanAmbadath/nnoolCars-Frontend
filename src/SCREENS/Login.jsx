290d1e13b6f994d8448fb2f0262e83c4ab4efbd7
SafvanAmbadath <safvanambadath17@gmail.com>
2023-06-26 11:34:18 +0530

    login with google account done

--- src/SCREENS/Login.jsx
+++ src/SCREENS/Login.jsx
@@ -2,20 +2,23 @@ import React, { useState } from "react";
 import Navbar from "../COMPONENTS/Navbar";
 // import { userAxios } from "../axiosLink/axios";
 // import { message } from "antd";
-import axios from "axios"
+import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import styles from "./styles.modules.css";
+import { LoginSocialGoogle } from "reactjs-social-login";
+import { GoogleLoginButton } from "react-social-login-buttons";
 
 function Login() {
     const [displayregister, setDisplayRegister] = useState(false);
     const [displaylogin, setDisplayLogin] = useState(true);
 
     const [data, setdata] = useState("");
-    const [logindata, setLoginData] = useState({email:"",password:""});
+    const [logindata, setLoginData] = useState({ email: "", password: "" });
 
     const navigate = useNavigate();
     const [error, setError] = useState("");
-    const [msg,setMsg]=useState("")
+    const [msg, setMsg] = useState("");
+
 
     const signUpHandle = (e) => {
         setdata({ ...data, [e.target.name]: e.target.value });
@@ -46,18 +49,18 @@ function Login() {
     //         message.error("fill columns");
     //     }
     // };
-    const submitLogin=async(e)=>{
-      try{
-        const url="http://localhost:4000/api/auth"
-        const {data:res}=await axios.post (url,logindata);
-        localStorage.setItem("token",res.data)
-        window.location="/home"
-      }catch(error){
-          if(error.response && error.response.status >= 400 && error.response.status <=500){
-            setError(error.response.data.message)
-          }
-      }
-    }
+    const submitLogin = async (e) => {
+        try {
+            const url = "http://localhost:4000/api/auth";
+            const { data: res } = await axios.post(url, logindata);
+            localStorage.setItem("token", res.data);
+            window.location = "/home";
+        } catch (error) {
+            if (error.response && error.response.status >= 400 && error.response.status <= 500) {
+                setError(error.response.data.message);
+            }
+        }
+    };
 
     // const submitSignup =async ()=>{
 
@@ -81,7 +84,7 @@ function Login() {
     //           message.error('Password incorrect')
 
     //       }
-          // console.log(msg);
+    // console.log(msg);
 
     //   }else{
     //     message.error('fill columns')
@@ -96,7 +99,7 @@ function Login() {
             const { data: res } = await axios.post(url, data);
             // navigate("/login");
             // console.log(res.message);
-            setMsg(res.message)
+            setMsg(res.message);
         } catch (error) {
             if (error.response && error.response.status >= 400 && error.response.status <= 500) {
                 setError(error.response.data.message);
@@ -104,16 +107,51 @@ function Login() {
         }
     };
 
+    const emailverify = async(data)=>{
+        try {
+            console.log("google login");
+            console.log(data);
+            const url="http://localhost:4000/api/users/googleAuthentication"
+            // console.log(url);
+            const res = await axios.post(url,data)
+            console.log(res);
+            if (res.status === 200) {
+                console.log("status true");
+            localStorage.setItem("token", res.data.token);
+            window.location="/home"
 
-
+            //   const result = res.data;
+            //   dispatch(
+            //     setLogin({
+            //       ...oldDate,
+            //       user: "user",
+            //       id: result.id,
+            //       name: result.name,
+            //       token: result.token,
+            //       email: result.email,
+            //       number: result.number,
+            //     })
+            //   );
+            //   if (oldDate.location && oldDate.endDate) {
+            //     navigate("/home");
+            //   } else if (oldDate.location) {
+            //     navigate("/place");
+            //   } else {
+            //     navigate("/");
+            //   }
+            // } else {
+            }
+          } catch {}
+        
+    }
 
     return (
         <div className="w-screen h-screen bg-LandingImage">
             <Navbar />
             <div className="w-full text-center items-center flex justify-center">
                 <div className="bg-white rounded-lg mt-10 h-[500px] w-[470px] items-center p-7  ">
-                {error && <div className={styles.error_msg}>{error}</div>}
-                {msg && <div className={styles.success_msg}>{msg}</div>}
+                    {error && <div className={styles.error_msg}>{error}</div>}
+                    {msg && <div className={styles.success_msg}>{msg}</div>}
                     <div className="w-full flex">
                         <div className="w-1/2 mt-4">
                             <div
@@ -177,10 +215,25 @@ function Login() {
                                     </button>
                                 </div>
                                 <h1 className="w-full text-center">OR</h1>
-                                <div className=" -mt">
+                                {/* <div className=" -mt">
                                     <button className="w-full rounded-lg h-12 bg-slate-100 mt-  pl-4 font-semibold">
                                         Login with OTP
                                     </button>
+                                </div> */}
+                                <div className="h-28 items-center mt-2  ">
+                                    <LoginSocialGoogle
+                                        className="  w-400 items-center  text-white rounded-md h-16  mb-10"
+                                        client_id="271994734148-prgm6lkk9kn4bmj1g2u17care9ff717g.apps.googleusercontent.com"
+                                        scope="openid profile email"
+                                        discoveryDocs="claims_supported"
+                                        access_type="offline"
+                                        onResolve={emailverify}
+                                        onReject={(err) => {
+                                            console.log(err);
+                                        }}
+                                    >
+                                        <GoogleLoginButton className="bg-dark-purple w-400   text-white rounded-md h-  mb-10" />
+                                    </LoginSocialGoogle>
                                 </div>
                             </div>
                         </>
